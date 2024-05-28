a73392fc2bd39409766f76dc59acc817f21ce2a2
anastasya-777 <152620467+anastasya-777@users.noreply.github.com>
2024-05-28 23:53:48 +0300

    Update BonusServiceTest.java

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -15,7 +15,7 @@ public class BonusServiceTest {
         long actual = service.calculate(amount, registered);
 
         // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
 
     @org.junit.jupiter.api.Test
@@ -31,7 +31,7 @@ public class BonusServiceTest {
         long actual = service.calculate(amount, registered);
 
         // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
 
     @org.junit.jupiter.api.Test
@@ -44,7 +44,7 @@ public class BonusServiceTest {
 
         long actual = service.calculate(amount, registered);
 
-        assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
 
     @org.junit.jupiter.api.Test
@@ -57,7 +57,7 @@ public class BonusServiceTest {
 
         long actual = service.calculate(amount, registered);
 
-        assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
 
     @org.junit.jupiter.api.Test
@@ -70,7 +70,7 @@ public class BonusServiceTest {
 
         long actual = service.calculate(amount, registered);
 
-        assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
 
      @org.junit.jupiter.api.Test
@@ -83,7 +83,7 @@ public class BonusServiceTest {
 
         long actual = service.calculate(amount, registered);
 
-        assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
 
      @org.junit.jupiter.api.Test
@@ -96,7 +96,7 @@ public class BonusServiceTest {
 
         long actual = service.calculate(amount, registered);
 
-        assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
 
 
